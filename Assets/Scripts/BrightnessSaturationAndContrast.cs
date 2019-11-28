28e76edb74719f6badcef110c3b112bda579d133
huangwenguang <huangwenguang@corp.netease.com>
2019-11-28 21:12:28 +0800

    开始后处理部分学习，加入亮度饱和度对比度调整的post effects实现

---
+++ Assets/Scripts/BrightnessSaturationAndContrast.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightnessSaturationAndContrast : PostEffectsBase
+{
+    public Shader briSatConShader;
+    private Material briSatConMaterial;
+
+    [Range(0.0f, 3.0f)]
+    public float brightness = 1.0f;
+
+    [Range(0.0f, 3.0f)]
+    public float saturation = 1.0f;
+
+    [Range(0.0f, 3.0f)]
+    public float contrast = 1.0f;
+
+    public Material material
+    {
+        get
+        {
+            briSatConMaterial = CheckShaderAndCreateMaterial(briSatConShader, briSatConMaterial);
+            return briSatConMaterial;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null)
+        {
+            material.SetFloat("_Brightness", brightness);
+            material.SetFloat("_Saturation", saturation);
+            material.SetFloat("_Contrast", contrast);
+
+            Graphics.Blit(src, dest, material);
+        }
+        else {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
