0b5ec4e0da700a633d4c9d9a8b993abbcf3fba96
huangwenguang <huangwenguang@corp.netease.com>
2019-11-29 12:24:39 +0800

    增加边缘检测后处理的实现

---
+++ Assets/Scripts/EdgeDetection.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeDetection : PostEffectsBase
+{
+    public Shader edgeDetectionShader;
+    private Material edgeDetectionMaterial = null;
+    public Material material
+    {
+        get
+        {
+            edgeDetectionMaterial = CheckShaderAndCreateMaterial(edgeDetectionShader, edgeDetectionMaterial);
+            return edgeDetectionMaterial;
+        }
+    }
+
+    [Range(0.0f, 1.0f)]
+    public float edgesOnly = 0.0f;
+
+    public Color edgeColor = Color.black;
+
+    public Color backgroundColor = Color.white;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null)
+        {
+            material.SetFloat("_EdgesOnly", edgesOnly);
+            material.SetColor("_EdgeColor", edgeColor);
+            material.SetColor("_BackgroundColor", backgroundColor);
+
+            Graphics.Blit(src, dest, material);
+        }
+        else {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
